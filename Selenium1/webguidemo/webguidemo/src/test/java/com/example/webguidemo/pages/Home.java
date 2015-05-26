89752e778653865d6df14ebe977da684d92e83bd
laikkk <kamilamd@gmail.com>
2015-05-26 19:15:14 +0200

    Add Selenium1 project

---
+++ Selenium1/webguidemo/webguidemo/src/test/java/com/example/webguidemo/pages/Home.java
@@ -0,0 +1,28 @@
+package com.example.webguidemo.pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.jbehave.web.selenium.WebDriverPage;
+import org.jbehave.web.selenium.WebDriverProvider;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class Home extends WebDriverPage {
+
+	@FindBy(how = How.ID, using = "loginregisterToGarage")
+	WebElement signIn;
+	
+	public Home(WebDriverProvider driverProvider) {
+		super(driverProvider);
+	}
+
+	public void open() {
+		get("https://my-garage.azurewebsites.net/");
+		manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+	
+	public void openLoginPage(){
+		signIn.click();
+	}
+}
